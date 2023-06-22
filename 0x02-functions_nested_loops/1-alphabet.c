daa9e01dc41dddbe5b2b6a635916c71b96cf9e6f
Keabetsoe25 <mathobelakabelo8@gmail.com>
2023-06-21 19:16:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  */
 
 void print_alphabet(void)
-{	
+{
 	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-                 _putchar(ch);
+	for  (ch = 'a'; ch = < 'z'; ch++)
+		_putchar(ch);
 
-        _putchar('\n');
-}	
+	_putchar('\n');
+}
